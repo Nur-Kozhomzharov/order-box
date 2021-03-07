2a9b797e91526c86f62f4887e9d3f32b218198e0
Nur-Kozhomzharov <76613170+Nur-Kozhomzharov@users.noreply.github.com>
2021-03-07 14:12:08 +0600

    uploaded files
    HTML, CSS, JAVASCRIPT.

---
+++ data.js
@@ -0,0 +1,39 @@
+const menu_items = [{
+    id: 1,
+    title: 'Hamburger',
+    price: 15,
+    img: './images/hamburger.png'
+},
+{
+    id: 2,
+    title: 'Coffee',
+    price: 14,
+    img: './images/coffee.png'
+},
+{
+    id: 3,
+    title: 'Cola',
+    price: 12,
+    img: './images/cola.svg'
+},
+{
+    id: 4,
+    title: 'Tea',
+    price: 10,
+    img: './images/tea.svg'
+},
+{
+    id: 5,
+    title: 'Cheeseburger',
+    price: 16,
+    img: './images/cheeseburger.svg'
+},
+{
+    id: 6,
+    title: 'Fries',
+    price: 11,
+    img: './images/fries.svg'
+}
+]
+
+let orders_basket = []
